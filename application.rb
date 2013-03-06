20a2c00033e15a1992c8e3ac8edbd59e616927aa
Rifki fauzi <kubido@gmail.com>
2013-03-06 08:23:00 +0700

    require sqlite3 on development only

--- application.rb
+++ application.rb
@@ -1,14 +1,14 @@
 require 'sinatra'
 require 'haml'
-require 'sqlite3'
 require "sinatra/activerecord"
 require './app/helpers/uproject'
-
+require 'sqlite3' if Sinatra::Base.development?
 enable :sessions
 
 set :database, "sqlite3:///uproject.db"
 
 class Application
+  
   %w(models controllers helpers).map { |p| Dir.glob("#{Dir.pwd}/app/#{p}/*.rb") { |m| require "#{m.chomp}" }}
   configure do
     set :method_override, true 
