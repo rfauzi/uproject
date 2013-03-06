d9737162386892aa87fbcf96c7419a41dc24cdbd
Rifki fauzi <kubido@gmail.com>
2013-03-06 08:47:06 +0700

    change db structure

--- application.rb
+++ application.rb
@@ -2,14 +2,30 @@ require 'sinatra'
 require 'haml'
 require "sinatra/activerecord"
 require './app/helpers/uproject'
-require 'sqlite3' if Sinatra::Base.development?
 enable :sessions
 
-set :database, "sqlite3:///uproject.db"
+
 
 class Application
-  
   %w(models controllers helpers).map { |p| Dir.glob("#{Dir.pwd}/app/#{p}/*.rb") { |m| require "#{m.chomp}" }}
+  configure :development, :test do
+    set :database, "sqlite3:///uproject.db"
+  end
+
+  configure :production do
+    # Database connection
+    db = URI.parse(ENV['DATABASE_URL'])
+
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
+
   configure do
     set :method_override, true 
     set :views, settings.root + '/app/views'
