a9b3ae7ccca1bafb12afeb21de5f36525dea7a90
Rifki fauzi <kubido@gmail.com>
2013-03-11 02:39:03 +0700

    mesgssage must uniq

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,12 @@ class Message < ActiveRecord::Base
 
   validates_presence_of :message, :message => "Harus diisi"
   validates_length_of :message, :maximum => 60
-  validates_uniqueness_of :message
+  # validate :identical_with_previous, :message => "Message is identical to the previous one"
+
+
+  # def identical_with_previous
+  #   last_message = self.user.messages.last
+  #   identical = last_message.present? ? self.message == last_message.message : false
+  #   return !identical
+  # end  
 end
