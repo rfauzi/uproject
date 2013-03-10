ecd89b79cdf6917e64f3d7e259081b203b2d223b
Rifki fauzi <kubido@gmail.com>
2013-03-11 02:34:31 +0700

    msage must uniq

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,5 @@ class Message < ActiveRecord::Base
 
   validates_presence_of :message, :message => "Harus diisi"
   validates_length_of :message, :maximum => 60
+  validates_uniqueness_of :message
 end
