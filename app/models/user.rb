c49f824a6c00f011de25f45c67b229dde95f5c6e
Rifki fauzi <kubido@gmail.com>
2013-03-11 23:01:11 +0700

    validate user attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_one   :profile
   has_many  :messages
   
+  validates_presence_of :email, :username, :uid
+
   def self.register_facebook(params)
     @user= User.new(username: params[:username], email: params[:email], uid: params[:uid], descriptions: params[:bio])
     return @user.save
