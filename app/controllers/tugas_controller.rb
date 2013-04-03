64cb4caa6ece2926d8cb528492a905fde12633f0
Rifki fauzi <kubido@gmail.com>
2013-04-03 22:01:21 +0700

    tugas

--- app/controllers/tugas_controller.rb
+++ app/controllers/tugas_controller.rb
@@ -3,4 +3,8 @@ class TugasController < Application
   get "/tugas_1" do 
     haml :'tugas/tugas_1', :layout => false
   end
+
+  get "/tugas_2" do
+    haml :'tugas/tugas_2', :layout => false
+  end
 end
